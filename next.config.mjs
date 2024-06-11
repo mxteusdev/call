66a97161e2446bb58a5f5edfdaa22ac671c8c51a
Mateus Rodrigues <mateusjaed@gmail.com>
2024-06-11 13:43:04 -0300

    initial config, stylesglobal, index and dot pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  pageExtensions: ['page.tsx', 'api.ts', 'api.tsx'],
+}
 
-export default nextConfig;
+export default nextConfig
