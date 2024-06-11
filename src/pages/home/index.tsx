4cad65c6086e00028b876f6159d4904cd66a74dd
Mateus Rodrigues <mateusjaed@gmail.com>
2024-06-11 14:49:53 -0300

    added button component from home screen and fixed image of preview app

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -3,18 +3,21 @@ import Image from 'next/image'
 import { Container, Hero, Preview } from './styles'
 
 import previewImage from '../../assets/app-preview.png'
+import { ClaimUsernameForm } from './components/ClaimUsernameForm'
 
 export default function Home() {
   return (
     <Container>
       <Hero>
         <Heading as="h1" size="4xl">
-          Agendamento descomplicado.
+          Agendamento descomplicado
         </Heading>
         <Text size="xl">
           Conecte seu calendário e permita que as pessoas marquem agendamentos
           no seu tempo livre.
         </Text>
+
+        <ClaimUsernameForm />
       </Hero>
 
       <Preview>
